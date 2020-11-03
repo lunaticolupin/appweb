b85306d373c47ed95c9a2a568abea3b50fb385f6
Alejandro Ramirez <lunaticolupin@outlook.com>
2020-11-03 12:46:16 -0600

    Update

--- bd/update.php
+++ bd/update.php
@@ -26,20 +26,18 @@ if(isset($_POST['enviar'])){
 }
 
 $id = $_GET["id"];
-
 $sql = "select * from noticias where id = {$id}";
 
 $temp = read($db, $sql);
-
 $resultado = $temp[0];
 ?>
-<h1>Nueva noticia</h1>
+<h1>Actualizar noticia</h1>
 <form method="post">
     <p><input type="number" name="id" value="<?php echo $resultado["id"]?>" readonly />
     <p><input type="texto" name="titulo" placeholder="Titulo de la nota" value="<?php echo $resultado["titulo"]?>"  required></p>
     <p><textarea name="nota" required><?php echo $resultado["texto"]?></textarea></p>
     <p><input type="texto" name="categoria" placeholder="categoria de la nota" value="<?php echo $resultado["categoria"]?>"></p>
     <p><input type="datetimelocal" name="fecha" required value="<?php echo $resultado["fecha"]?>"></p>
-    <p><input type="submit" name="enviar" value="Crear"></p>
+    <p><input type="submit" name="enviar" value="Actualizar"></p>
 </form>
 <a href="index.php">Atras</a>
